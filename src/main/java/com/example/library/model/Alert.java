d91a39fcbb40f69c82c8e6e889f9ba9b7c9dc48a
thanhphuocjr <thanhphuocjr03012005@gmail.com>
2024-11-27 17:16:31 +0700

    Alert Timne out

--- src/main/java/com/example/library/model/Alert.java
+++ src/main/java/com/example/library/model/Alert.java
@@ -1,5 +1,10 @@
 package com.example.library.model;
 
+import javafx.animation.PauseTransition;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
 public class Alert {
     public static void showInfoAlert(String title, String header, String content) {
         javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
@@ -23,4 +28,22 @@ public class Alert {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    public static void temptLabel(Label label, String message, Color color) {
+        // Cập nhật nội dung của Label
+        label.setText(message);
+
+        // Cập nhật màu sắc
+        label.setTextFill(color);
+
+        // Hiển thị Label
+        label.setVisible(true);
+
+        // Sử dụng PauseTransition để ẩn Label sau khoảng thời gian
+        PauseTransition pause = new PauseTransition(Duration.seconds(2.5));
+        pause.setOnFinished(event -> label.setVisible(false));
+        pause.play();
+    }
+
+
 }
